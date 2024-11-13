8294730ed3d84a8ab968d00369d9723e7dd499ba
naresh <nareshbn2807@gmail.com>
2024-11-13 11:17:09 +0530

    hi

--- demo.java
+++ demo.java
@@ -2,5 +2,7 @@ public class{
   public static void main(String [] args){
   system.out.println("Hello world");
 system.out.println("2nd modification")
+  system.out.println("----------------------------");
+system.out.println("2nd modification-------------------")
 }
 }
